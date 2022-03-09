d6e56cde2a1c99c3745b0976188ba9b249309eb1
manisha <manisha.verma23@gmail.com>
2022-03-09 12:25:53 +0530

    setting up our sign up page and getting the data that's inside

--- app.js
+++ app.js
@@ -5,12 +5,19 @@ const bodyParser = require("body-parser");
 
 const app = express();                // app is an instance of express
 app.use(express.static("public"));    //way to keep all the the static files separate in public folder
+app.use(bodyParser.urlencoded({extended:true}));  // use body-parser.urlencoded to get post request from html files
+
 
 app.get("/", function(req,res){
     res.sendFile(__dirname + "/signup.html");
 });
 
-
+app.post("/", function(req,res){
+    var firstName = req.body.fName;
+    var lastName = req.body.lName;
+    var email = req.body.emailAddress;
+    console.log(firstName , lastName, email);
+});
 
 
 app.listen(3000 , function(){
